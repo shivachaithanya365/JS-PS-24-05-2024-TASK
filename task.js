182a22ff96c5036f02abf3dd85a9db495e71a458
alonewalkerchaithu <shivachaithanya365@gmail.com>
2024-07-06 11:43:44 +0530

    Updated

--- task.js
+++ task.js
@@ -1,9 +1,9 @@
 // 1. Write a program to print even numbers in the given range (1, input num) -side by side
 // input: Enter a number: 10  //output: 2 4 6 8 10
-num = +prompt("Enter a number : ");
+var num = +prompt("Enter a number : ");
 // let num = 10;
 s = ""
-for (i = 1; i <= num; i++) {
+for (let i = 1; i <= num; i++) {
     if (i % 2 == 0) {
         s = s + i + " ";
     }
@@ -13,11 +13,11 @@ console.log(`Even numbers from 1 to given input ( ${num} ) is : ${s}`);         
 
 // 2. Write a program to print sum of odd numbers in the fibonacci series
 // input: Enter no of series : 5 (0 1 1 2 3)   //output: 5
-num = +prompt("Enter a number :")
+var num = +prompt("Enter a number :")
 // let num = 5;
-let a = 0;
-let b = 1;
-let sum = 0;
+var a = 0;
+var b = 1;
+var sum = 0;
 if (num == 0) {
     document.write(`<h2>( ${num} ) is INVALID INPUT</h2>`)
     console.log("INVALID INPUT");
@@ -43,11 +43,11 @@ console.log(`Sum of odd numbers in the fibonacci series ( ${num} ):  ${sum}`);
 
 // 3. Write a program to print sum of odd numbers in the fibonacci series and number should be added only once
 // input: Enter no of series : 5   //output: 4
-num = +prompt("Enter no. of series :")
+var num = +prompt("Enter no. of series :")
 // num = 5;
-a = 0;
-b = 1;
-sum = 0;
+var a = 0;
+var b = 1;
+var sum = 0;
 if (num == 0) {
     document.write(`<h2> ( ${num} ) is INVALID INPUT</h2>`)
     console.log(`<h2>INVALID INPUT</h2>`);
@@ -72,12 +72,12 @@ document.write(`<h2>Sum of odd numbers in the fibonacci series (without duplicat
 console.log(`Sum of odd numbers in the fibonacci series:(without duplicates) ( ${num} ) is : + ${sum - 1}`);
 
 //method - 2
-num = +prompt("Enter no. of series :")
+var num = +prompt("Enter no. of series :")
 // num = 5;
-a = 0;
-b = 1;
-sum = [];
-add = 0;
+var a = 0;
+var b = 1;
+var sum = [];
+var add = 0;
 if (num == 0) {
     document.write(`<h2> ( ${num} ) is INVALID INPUT</h2>`)
     console.log("INVALID INPUT");
@@ -99,7 +99,7 @@ else {
         b = c;
         i++
     }
-    for (i of sum) {
+    for (let i of sum) {
         add = add + i;
     }
 }                                           //Sum of odd numbers in the fibonacci series: 4
@@ -108,11 +108,11 @@ console.log(`Sum of odd numbers in the fibonacci series:(without duplicates) ( $
 
 // 4. Write a program to print the below series based on input
 // Enter a number: -10   // output: -10 -9 -8 - 7 -6 - 5 - 4 -3 -2 -1
-number = +prompt("Enter a number(negative value) :")
+var number = +prompt("Enter a number(negative value) :")
 // number = -10;
 if (number < 0) {
     s = "";
-    for (i = number; i <= -1; i++) {
+    for (let i = number; i <= -1; i++) {
         s = s + i + " ";
     }
     console.log(`( ${number} ) is : ${s}`);                              //-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 
@@ -124,10 +124,10 @@ else {
 }
 
 // Enter a number: -10 (should not use <= operator)
-number = +prompt("Enter a number(negative value) :")
+var number = +prompt("Enter a number(negative value) :")
 // number = -10;
-s = "";
-for (i = number; !(i >= 0); i++) {
+var s = "";
+for (let i = number; !(i >= 0); i++) {
     s = s + i + " ";
 }
 document.write(`<h2>( ${number} ) is : ${s}<br><br></h2>`)
@@ -135,20 +135,20 @@ console.log(`( ${number} ) is : ${s}`);                              //-10 -9 -8
 
 // 5. Write a program to print the below series based on input
 // Enter a number: -10   //output: -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 
-number = +prompt("Enter a number(negative value) :")
+var number = +prompt("Enter a number(negative value) :")
 // number = -10;
-s = "";
-for (i = -1; i >= number; i--) {
+var s = "";
+for (let i = -1; i >= number; i--) {
     s = s + i + " ";
 }
 document.write(`<h2>( ${number} ) is : ${s}<br><br></h2>`)
 console.log(`( ${number} ) is : ${s}`);                              //-1 -2 -3 -4 -5 -6 -7 -8 -9 -10
 
 // Enter a number: -10 (should not use >= operator)
-number = +prompt("Enter a number(negative value) :")
+var number = +prompt("Enter a number(negative value) :")
 // number = -10;
-s = "";
-for (i = -1; !(i <= number - 1); i--) {
+var s = "";
+for (let i = -1; !(i <= number - 1); i--) {
     s = s + i + " ";
 }
 document.write(`<h2>( ${number} ) is : ${s}<br><br></h2>`)
